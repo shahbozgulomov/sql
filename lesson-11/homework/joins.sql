815d04bf808fb6800fc13699bab595d7482e5f7c
shahbozgulomov <77021529+shahbozgulomov@users.noreply.github.com>
2025-02-22 15:39:45 +0000

    Update joins.sql

--- lesson-11/homework/joins.sql
+++ lesson-11/homework/joins.sql
@@ -1,1 +1,10 @@
+select customer_name,sum(total_amount) from customers c
+join orders o
+on c.customer_id = o.customer_id
+group by customer_name
 
+select customer_name,sum(total_amount) as total_spent from customers c
+join orders o
+on c.customer_id = o.customer_id 
+group by customer_name
+having total_spent>(select avg(total_amount) from orders)
