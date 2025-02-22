5c143fb1d83ca3bc69977589cbbab38a6ca2c0a2
shahbozgulomov <77021529+shahbozgulomov@users.noreply.github.com>
2025-02-22 17:13:43 +0000

    Update joins.sql

--- lesson-11/homework/joins.sql
+++ lesson-11/homework/joins.sql
@@ -3,8 +3,14 @@ join orders o
 on c.customer_id = o.customer_id
 group by customer_name
 
-select customer_name,sum(total_amount) as total_spent from customers c
-join orders o
-on c.customer_id = o.customer_id 
-group by customer_name
-having total_spent>(select avg(total_amount) from orders)
+select a.customer_name from(
+           select o.order_id,total_amount,customer_name from orders o
+           join customers c
+           on o.customer_id = c.customer_id) as a
+join 
+          (
+           select c.customer_name,sum(total_amount) as t_amount from customers c
+           join orders o
+           on c.customer_id = o.customer_id
+           group by c.customer_name) as b
+on a.customer_name = b.customer_name and t_amount >(select avg(total_amount) from orders)
