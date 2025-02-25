9e017834f79c651f5342b5c5cc0f2b9b62b95b42
shahbozgulomov <77021529+shahbozgulomov@users.noreply.github.com>
2025-02-25 15:40:28 +0000

    Update joins.sql

--- lesson-11/homework/joins.sql
+++ lesson-11/homework/joins.sql
@@ -1,16 +1,18 @@
-select customer_name,sum(total_amount) from customers c
-join orders o
-on c.customer_id = o.customer_id
-group by customer_name
 
-select a.customer_name from(
-           select o.order_id,total_amount,customer_name from orders o
-           join customers c
-           on o.customer_id = c.customer_id) as a
-join 
-          (
-           select c.customer_name,sum(total_amount) as t_amount from customers c
-           join orders o
-           on c.customer_id = o.customer_id
-           group by c.customer_name) as b
-on a.customer_name = b.customer_name and t_amount >(select avg(total_amount) from orders)
+select * from employees e join departments d on e.DepartmentID = d.DepartmentID 
+
+select * from students s left join classes c on s.ClassID = c.ClassID 
+
+select * from orders o right join customers c on o.CustomerID = c.CustomerID
+
+select * from products p full outer join sales s on p.ProductID = s.ProductID 
+
+select e1.name,e2.name from Employees1 e1 join Employees1 e2 on e2.EmployeeID = e1.ManagerID
+
+select * from colors cross join sizes
+
+select title, name,releaseyear from Movies m join actors a on m.MovieID = a.MovieID and releaseyear > 2015
+
+select * from Orders2 o join customers1 c on o.CustomerID = c.CustomerID join OrderDetails od on o.OrderID = od.OrderID 
+
+select s.ProductID,sum(price) as total_revenue from sales1 s join Products1 p on s.ProductID = p.ProductID group by s.ProductID
