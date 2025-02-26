44c4602474a15cf534f32dea44686f04a80da52c
Shahboz Gulomov <77021529+shahbozgulomov@users.noreply.github.com>
2025-02-26 06:54:36 +0000

    Update sql.lesson-12.sql

--- lesson-12/homework/sql.lesson-12.sql
+++ lesson-12/homework/sql.lesson-12.sql
@@ -81,6 +81,74 @@ select Ceiling(4.57)
 select Current_Timestamp
 
 select DateName(weekday,'2025-02-03') 
+  
+SELECT REPLACE(REVERSE('SQL SERVER'), ' ', '') AS ReversedStringWithoutSpaces;
+
+SELECT STRING_AGG(City, ', ') AS ConcatenatedCities
+FROM Customers;
+
+DECLARE @str NVARCHAR(100) = 'SQL Server is a relational database management system';
+
+SELECT 
+    CASE 
+        WHEN CHARINDEX('SQL', @str) > 0 AND CHARINDEX('Server', @str) > 0 
+        THEN 'Both substrings found'
+        ELSE 'One or both substrings not found'
+    END AS Result;
+
+	select power(5,3)
+
+
+SELECT value AS Fruit
+FROM STRING_SPLIT('apple;orange;banana', ';');
+
+SELECT TRIM(' sql') AS TrimmedString;
+
+DECLARE @StartTime DATETIME = '2025-02-25 12:00:00';
+DECLARE @EndTime DATETIME = '2025-02-26 18:00:00';
+SELECT DATEDIFF(HOUR, @StartTime, @EndTime) AS HoursDifference;
+
+DECLARE @startDate DATE = '2023-05-01';
+DECLARE @endDate DATE = '2025-02-03';
+
+SELECT (DATEPART(YEAR, @endDate) - DATEPART(YEAR, @startDate)) * 12 +
+       (DATEPART(MONTH, @endDate) - DATEPART(MONTH, @startDate)) AS MonthsDifference;
+
+DECLARE @str NVARCHAR(100) = 'apple,orange,banana';
+
+SELECT value AS Fruit
+FROM STRING_SPLIT(@str, ',');
+
+DECLARE @startDate DATE = '2025-01-01';
+DECLARE @currentDate DATE = GETDATE();
+
+SELECT DATEDIFF(DAY, @startDate, @currentDate) AS DaysDifference;
+
+DECLARE @str NVARCHAR(100) = 'Data Science';
+SELECT LEFT(@str, 4) AS FirstFourCharacters;
+
+SELECT CEILING(SQRT(225)) AS RoundedSquareRoot;
+
+DECLARE @str1 NVARCHAR(50) = 'Hello';
+DECLARE @str2 NVARCHAR(50) = 'World';
+
+SELECT CONCAT_WS('|', @str1, @str2) AS ConcatenatedString;
+
+DECLARE @str NVARCHAR(100) = 'abc123xyz';
+SELECT PATINDEX('%[0-9]%', @str) AS FirstDigitPosition;
+
+select charindex('sql','sql server sql',(charindex('sql','sql server sql'))+1)
+
+SELECT DATEPART(YEAR, GETDATE()) AS CurrentYear;
+
+
+SELECT DATEADD(DAY, -100, GETDATE()) AS DateMinus100Days;
+
+SELECT DATENAME(WEEKDAY, '2025-02-03') AS DayOfWeek;
+
+DECLARE @number INT = 5;
+
+SELECT POWER(@number, 2) AS SquareOfNumber;
 
 
 
